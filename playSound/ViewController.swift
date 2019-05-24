b295a971ee056cd178d3f2c18471635f0ce5a572
mickey <pandamickey0425@gmail.com>
2019-05-24 13:43:49 +0800

    Initial Commit

---
+++ playSound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playSound
+//
+//  Created by Mickey on 2019/5/24.
+//  Copyright © 2019 Mickey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
